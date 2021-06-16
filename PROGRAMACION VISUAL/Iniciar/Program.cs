b7dc06fb1c8fadc5da93717b0c950eb97fa1544e
PILLAJO-HERNAN <52302923+khpm-1roa@users.noreply.github.com>
2021-06-16 00:23:55 -0500

    Update Program.cs

--- PROGRAMACION VISUAL/Iniciar/Program.cs
+++ PROGRAMACION VISUAL/Iniciar/Program.cs
@@ -6,7 +6,7 @@ namespace Iniciar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo");
         }
     }
 }
